1de1e9e2107d9c9d1645810e34e71622174ec020
boemsu <127827852+boemsu@users.noreply.github.com>
2024-03-13 10:27:00 +0900

    Add files via upload

---
+++ git_for_lean.py
@@ -0,0 +1,30 @@
+#<Q1>
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print(i,"*",j,"=",i*j)
+
+
+#<Q2>
+# a = int(input())
+# sum = 0
+
+# for i in range(1,a+1):
+#     sum += i
+
+# print(sum)
+
+#<Q3>
+# n = int(input())
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*")
+
+#Q4
+ps = str(input("암호를 입력하시오: "))
+
+while ps == "python":
+    print("로그인 성공")
+    break
+
+
